32658eebdfb2ba6dd06d40b6e90876df86a26f39
sssyntax <syu3887682@gmail.com>
2025-08-02 10:24:31 +0800

    Initial Commit

---
+++ stickman-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stickman-app
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stickman-app/stickman_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickman_appApp.swift
+//  stickman-app
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stickman_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
